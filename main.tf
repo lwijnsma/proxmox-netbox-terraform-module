da792651d83d3860ecf512858d74b20c5b52eefd
Laurens Wijnsma <wijnsmalaurens@gmail.com>
2026-09-18 16:35:26 +0200

    Remove a-Z from version

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     proxmox = {
       source = "bpg/proxmox"
-      version = "0.113.1s"
-    }
+      version = "0.113.1"
+      }
     netbox = {
       source  = "e-breuninger/netbox"
       version = "5.8.0"
